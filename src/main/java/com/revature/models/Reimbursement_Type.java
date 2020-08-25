a095d93dd7ac3bfa90fb828ce399660826e3b248
Tanisi90 <furalpanda@yahoo.com>
2020-08-24 20:03:37 -0700

    started project made models

---
+++ src/main/java/com/revature/models/Reimbursement_Type.java
@@ -0,0 +1,90 @@
+package com.revature.models;
+
+import java.io.Serializable;
+
+public class Reimbursement_Type implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int reimb_type_id;
+	private String reimb_type;
+	
+	
+	public Reimbursement_Type() {
+		super();
+	}
+
+
+	public Reimbursement_Type(String reimb_type) {
+		super();
+		this.reimb_type = reimb_type;
+	}
+
+
+	public Reimbursement_Type(int reimb_type_id, String reimb_type) {
+		super();
+		this.reimb_type_id = reimb_type_id;
+		this.reimb_type = reimb_type;
+	}
+
+
+	public int getReimb_type_id() {
+		return reimb_type_id;
+	}
+
+
+	public void setReimb_type_id(int reimb_type_id) {
+		this.reimb_type_id = reimb_type_id;
+	}
+
+
+	public String getReimb_type() {
+		return reimb_type;
+	}
+
+
+	public void setReimb_type(String reimb_type) {
+		this.reimb_type = reimb_type;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((reimb_type == null) ? 0 : reimb_type.hashCode());
+		result = prime * result + reimb_type_id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement_Type other = (Reimbursement_Type) obj;
+		if (reimb_type == null) {
+			if (other.reimb_type != null)
+				return false;
+		} else if (!reimb_type.equals(other.reimb_type))
+			return false;
+		if (reimb_type_id != other.reimb_type_id)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reimbursement_Type [reimb_type_id=" + reimb_type_id + ", reimb_type=" + reimb_type + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/revature/models/Users.java
@@ -0,0 +1,208 @@
+package com.revature.models;
+
+import java.io.Serializable;
+
+public class Users implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int users_id;
+	private String username;
+	private String password;
+	private String first_name;
+	private String last_name;
+	private String user_email;
+	private int user_role_id;
+	
+	
+	public Users() {
+		super();
+	}
+
+
+	public Users(String username, String password, String first_name, String last_name,
+			String user_email, int user_role_id) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.user_email = user_email;
+		this.user_role_id = user_role_id;
+	}
+
+
+	public Users(int users_id, String username, String password, String first_name,
+			String last_name, String user_email) {
+		super();
+		this.users_id = users_id;
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.user_email = user_email;
+	}
+
+
+	public Users(int users_id, String username, String password, String first_name,
+			String last_name, String user_email, int user_role_id) {
+		super();
+		this.users_id = users_id;
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.user_email = user_email;
+		this.user_role_id = user_role_id;
+	}
+
+
+	public Users(String username, String password, String first_name, String last_name,
+			String user_email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.user_email = user_email;
+	}
+
+
+	public int getUsers_id() {
+		return users_id;
+	}
+
+
+	public void setUsers_id(int Users_id) {
+		this.users_id = Users_id;
+	}
+
+
+	public String getusername() {
+		return username;
+	}
+
+
+	public void setusername(String username) {
+		this.username = username;
+	}
+
+
+	public String getpassword() {
+		return password;
+	}
+
+
+	public void setpassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getfirst_name() {
+		return first_name;
+	}
+
+
+	public void setfirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+
+	public String getlast_name() {
+		return last_name;
+	}
+
+
+	public void setlast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+
+	public int getUser_role_id() {
+		return user_role_id;
+	}
+
+
+	public void setUser_role_id(int user_role_id) {
+		this.user_role_id = user_role_id;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		result = prime * result + users_id;
+		result = prime * result + ((user_email == null) ? 0 : user_email.hashCode());
+		result = prime * result + ((first_name == null) ? 0 : first_name.hashCode());
+		result = prime * result + ((last_name == null) ? 0 : last_name.hashCode());
+		result = prime * result + user_role_id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Users other = (Users) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		if (users_id != other.users_id)
+			return false;
+		if (user_email == null) {
+			if (other.user_email != null)
+				return false;
+		} else if (!user_email.equals(other.user_email))
+			return false;
+		if (first_name == null) {
+			if (other.first_name != null)
+				return false;
+		} else if (!first_name.equals(other.first_name))
+			return false;
+		if (last_name == null) {
+			if (other.last_name != null)
+				return false;
+		} else if (!last_name.equals(other.last_name))
+			return false;
+		if (user_role_id != other.user_role_id)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Users [users_id=" + users_id + ", username=" + username + ", password="
+				+ password + ", first_name=" + first_name + ", last_name=" + last_name
+				+ ", user_email=" + user_email + ", user_role_id=" + user_role_id + "]";
+	}
+	
+	
+	
+	
+}
